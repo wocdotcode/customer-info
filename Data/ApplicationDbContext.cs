6c49d24e9bda12b560540bbed0fffd2a8d386eb0
Wendell Centeno <dellortiz06@gmail.com>
2024-06-25 23:50:42 +0800

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomerInfo.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
